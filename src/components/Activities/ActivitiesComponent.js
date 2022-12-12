7396f86f2a87c20daacbf8a711f272230e2757f8
sam-rached <rachedsamir@gmail.com>
2022-12-12 09:34:11 +0100

    load images modifiy activitiescomponent

--- src/components/Activities/ActivitiesComponent.js
+++ src/components/Activities/ActivitiesComponent.js
@@ -1,15 +1,74 @@
-import React from 'react'
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import './ActivitiesComponent.css';
 
 function ActivitiesComponent() {
   return (
-    <div className='activitiesComponent'>
+    <div className="activitiesComponent">
       <div className="activities__list">
-        <div className='activity__box'>
-        
+        <div className="activity__box">
+          <NavLink to="ladiesFashion">
+            <img
+              src={
+                process.env.PUBLIC_URL +
+                `/imgs/majestic_ladies_fashion_division.jpg`
+              }
+              alt="activity picture"
+            />
+          </NavLink>
+          <div className="box__information">Ladies Fashion</div>
+        </div>
+        <div className="activity__box">
+          <NavLink to="kidsFashion">
+            <img
+              src={
+                process.env.PUBLIC_URL +
+                `/imgs/majestic_kids_fashion_division.jpg`
+              }
+              alt="activity picture"
+            />
+          </NavLink>
+          <div className="box__information">Kids Fashion</div>
+        </div>
+        <div className="activity__box">
+          <NavLink to="mensFashion">
+            <img
+              src={
+                process.env.PUBLIC_URL +
+                `/imgs/majestic_mens_fashion_division.jpg`
+              }
+              alt="activity picture"
+            />
+          </NavLink>
+          <div className="box__information">Mens Fashion</div>
+        </div>
+        <div className="activity__box">
+          <NavLink to="watches">
+            <img
+              src={
+                process.env.PUBLIC_URL +
+                `/imgs/majestic_fashion_watches_division.jpg`
+              }
+              alt="activity picture"
+            />
+          </NavLink>
+          <div className="box__information">Watches</div>
+        </div>
+        <div className="activity__box">
+          <NavLink to="rucolineShoes">
+            <img
+              src={
+                process.env.PUBLIC_URL +
+                `/imgs/majestic_fashion_accessories_division.jpg`
+              }
+              alt="activity picture"
+            />
+          </NavLink>
+          <div className="box__information">Rucoline Shoes</div>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Activities
+export default ActivitiesComponent
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import './Home.css';
 import Slider from '../../components/Slider/Slider';
+import ActivitiesComponent from '../../components/Activities/ActivitiesComponent';
 
 function Home() {
   return (
     <div className='home'>
         <Slider />
+        <ActivitiesComponent/>
     </div>
   )
 }
