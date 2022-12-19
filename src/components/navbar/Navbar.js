a8191fdffa4417ab1ad0a170a530e4f0aff4a3aa
sam-rached <rachedsamir@gmail.com>
2022-12-19 17:31:36 +0100

    modify app.css activityComponent.css informationsComponent.css navbar.css .js

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,53 +1,56 @@
-import React, {useState} from "react";
+import React, { useState, useEffect } from "react";
 import { NavLink } from "react-router-dom";
 import "./Navbar.css";
 import MainTitle from "../../assets/images/majestic-logo-2x-370x69-alpha.png";
 import MenuIcon from "@mui/icons-material/Menu";
 
 function Navbar() {
-    const [click, setClick] = useState(true);
-    const [menu, setMenu]=useState(true);
+  const [click, setClick] = useState(true);
+  const [menu, setMenu] = useState(true);
 
-    
-    const handleMenu =()=> {
-        setMenu(!menu);
+  const handleMenu = () => {
+    setMenu(!menu);
 
-        if (!click){
-            setClick(!click);
-        };
-
-        hideMenu()
+    if (!click) {
+      setClick(!click);
     }
 
-    const handleClick = () => setClick(!click);
+    hideMenu();
+  };
 
-    const hideMenu=()=>{
-      const menuTab = document.getElementById('menu');
-      menuTab.addEventListener('mouseleave',()=>{
-        setMenu(true);
-      })
+  const handleClick = () => setClick(!click);
+
+  const hideMenu = () => {
+    const menuTab = document.getElementById("menu");
+    menuTab.addEventListener("mouseleave", () => {
+      setMenu(true);
+    });
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", hideTop);
+
+    return () => {
+      window.removeEventListener("scroll", hideTop);
+    };
+  }, []);
+
+  const hideTop = () => {
+const {scrollTop} = document.documentElement;
+const navbar = document.querySelector('.navbar');
+const headerRight = document.querySelector('.right__header');
+console.log(scrollTop);
+    if (scrollTop > 24) {
+      headerRight.style.position='fixed';
     }
-
-    const scrollTop=()=>{
-      const navbar = document.querySelector(".navbar");
-      const {scrollTop, scrollHeight, clientHeight}=document.documentElement;
-
-
-    
-
+    else{
+        headerRight.style.position = "block";
     }
-    
-  
-
+  };
 
   return (
-    <div className="navbar" onChange={scrollTop}>
+    <div className="navbar">
       <header className="header">
-        <div onClick={handleMenu} className="menu__header">
-          <MenuIcon />
-          <button className="button__menu">Menu</button>
-        </div>
-        <img className="main__title" src={MainTitle} alt="logo" />
         <div className="right__header">
           <ul>
             <li>
@@ -57,6 +60,13 @@ function Navbar() {
               <NavLink to="contactUs">Contact Us</NavLink>
             </li>
           </ul>
+        </div>
+        <div className="container__menu">
+          <div onClick={handleMenu} className="menu__header">
+            <MenuIcon />
+            <button className="button__menu">Menu</button>
+          </div>
+          <img className="main__title" src={MainTitle} alt="logo" />
         </div>
       </header>
       <nav className={menu ? "nav" : "nav active"}>
